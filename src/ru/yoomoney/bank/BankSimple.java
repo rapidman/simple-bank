4b785408131251126625b9cdaa575797d38931b8
Timur Saidov <saidovtv@sibedge.com>
2023-05-17 23:32:33 +0700

    implement transfer method

--- src/ru/yoomoney/bank/BankSimple.java
+++ src/ru/yoomoney/bank/BankSimple.java
@@ -2,10 +2,8 @@ package ru.yoomoney.bank;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Класс представляет собой контракт на реализацию банка.
@@ -14,91 +12,116 @@ import java.util.Objects;
  */
 class BankSimple {
 
-    private final List<Account> accounts;
-    private final Map<Account, List<HistoryItem>> history;
+  private static final String ACCOUNT_NOT_FOUND_ERROR = "Account not found, accountId=%s";
+  private final List<Account> accounts;
+  private final Map<Account, List<HistoryItem>> history;
+
+  /**
+   * Конструктор
+   *
+   * @param accounts список счетов пользователей доступных в системе
+   * @param history  история операций по счетам
+   */
+  public BankSimple(List<Account> accounts, Map<Account, List<HistoryItem>> history) {
+    // TODO
+    this.accounts = accounts;
+    this.history = history;
+  }
+
+  /**
+   * Перевести средства со счёта одного пользователя на счёт другого пользователя
+   *
+   * @param senderId   идентификатор счёта отправителя
+   * @param receiverId идентификатор счёта получателя
+   * @param amount     сумма перевода
+   */
+  public void transfer(long senderId, long receiverId, BigDecimal amount) {
+    Account sender = getAccountById(senderId);
+    Account receiver = getAccountById(receiverId);
+    synchronized (sender) {
+      synchronized (receiver) {
+        sender.subtractAmount(amount);
+        receiver.addAmount(amount);
+      }
+    }
+  }
+
+  private Account getAccountById(long accountId) {
+    return accounts.stream()
+        .filter(account -> account.id == accountId).findAny()
+        .orElseThrow(() -> {
+          throw new RuntimeException(String.format(ACCOUNT_NOT_FOUND_ERROR, accountId));
+        });
+  }
+
+
+  /**
+   * Счёт пользователя
+   */
+  public static class Account {
 
     /**
-     * Конструктор
-     *
-     * @param accounts список счетов пользователей доступных в системе
-     * @param history  история операций по счетам
+     * Уникальный идентификатор счёта
      */
-    public BankSimple(List<Account> accounts, Map<Account, List<HistoryItem>> history) {
-        // TODO
-        this.accounts = accounts;
-        this.history = history;
+    long id;
+    /**
+     * Количество средств на счёте
+     */
+    BigDecimal balance;
+
+    public void addAmount(BigDecimal amount) {
+      balance = balance.add(amount);
     }
+
+    public void subtractAmount(BigDecimal amount) {
+      balance = balance.subtract(amount);
+    }
+  }
+
+  /**
+   * Запись в истории операций пользователя
+   */
+  public static class HistoryItem {
 
     /**
-     * Перевести средства со счёта одного пользователя на счёт другого пользователя
-     *
-     * @param senderId   идентификатор счёта отправителя
-     * @param receiverId идентификатор счёта получателя
-     * @param amount     сумма перевода
+     * Тип операции
      */
-    public void transfer(long senderId, long receiverId, BigDecimal amount) {
-        // TODO
-    }
-
+    OperationType type;
     /**
-     * Счёт пользователя
+     * Сумма операции
      */
-    public static class Account {
-        /**
-         * Уникальный идентификатор счёта
-         */
-        long id;
-        /**
-         * Количество средств на счёте
-         */
-        BigDecimal balance;
-
-    }
-
+    BigDecimal amount;
     /**
-     * Запись в истории операций пользователя
+     * Время проведения операции
      */
-    public static class HistoryItem {
+    LocalDateTime date;
 
-        /**
-         * Тип операции
-         */
-        OperationType type;
-        /**
-         * Сумма операции
-         */
-        BigDecimal amount;
-        /**
-         * Время проведения операции
-         */
-        LocalDateTime date;
-
-        public HistoryItem(OperationType type, BigDecimal amount, LocalDateTime date) {
-            this.type = type;
-            this.amount = amount;
-            this.date = date;
-        }
-
+    public HistoryItem(OperationType type, BigDecimal amount, LocalDateTime date) {
+      this.type = type;
+      this.amount = amount;
+      this.date = date;
     }
 
+  }
+
+  /**
+   * Тип операции в истории пользователя
+   */
+  public enum OperationType {
     /**
-     * Тип операции в истории пользователя
+     * Снятие средств
      */
-    public enum OperationType {
-        /**
-         * Снятие средств
-         */
-        WITHDRAW,
-        /**
-         * Пополнение средств
-         */
-        TOPUP
-    }
-
+    WITHDRAW,
     /**
-     * Период для расчёта статистики
+     * Пополнение средств
      */
-    public enum Period {
-        DAY, WEEK, MONTH
-    }
+    TOPUP
+  }
+
+  /**
+   * Период для расчёта статистики
+   */
+  public enum Period {
+    DAY, WEEK, MONTH
+  }
 }
